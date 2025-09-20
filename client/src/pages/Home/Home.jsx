6fb6297fb6f3d102bbf16464b7327cdb9285cb7e
Monther Alzamli <60818007+MontherIsmail@users.noreply.github.com>
2025-09-20 12:38:28 +0300

    Update Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,7 +7,6 @@ function Home() {
   const navigate = useNavigate();
   return (
     <div className="home-page">
-      <Logo />
       <div className="content">
         <p className="btn-shine">موقع مختص لتوصيل الطرود لكافة محافظات قطاع غزة , بطريقة آمنة ووقت قياسي</p>
         <button type="button" className="btn btn-primary m-0" onClick={() => navigate('./verify')}>ابدأ</button>
