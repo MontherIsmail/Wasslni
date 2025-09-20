a0d869f98ea2c53e4fe8712830dd0f6a6585f86f
Monther Alzamli <60818007+MontherIsmail@users.noreply.github.com>
2025-09-20 12:46:22 +0300

    Update Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,8 +7,9 @@ function Home() {
   const navigate = useNavigate();
   return (
     <div className="home-page">
+      <Logo />
       <div className="content">
-        <p className="btn-shine">موقع مختص لتوصيل الطرود لكافة محافظات قطاع غزة , بطريقة آمنة ووقت قياسي</p>{/* eslint-disable-line no-trailing-spaces */}
+        <p className="btn-shine">موقع مختص لتوصيل الطرود لكافة محافظات قطاع غزة , بطريقة آمنة ووقت قياسي</p>
         <button type="button" className="btn btn-primary m-0" onClick={() => navigate('./verify')}>ابدأ</button>
       </div>
     </div>
